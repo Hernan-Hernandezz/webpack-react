7ab54e6fd2d24c0808aaf43c5f1815070c07e7b5
Hernan-Hernandezz <andreshernandez0827@gmail.com>
2021-12-30 17:52:56 -0500

    :wrench: update files de configs

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,48 +1,65 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
-  entry: './src/index.js',
+  entry: "./src/index.js",
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+    publicPath: "/",
   },
   resolve: {
-    extensions: ['.js', '.jsx']
+    extensions: [".js", ".jsx"],
+    alias: {
+      "@styles": path.resolve(__dirname, "src/styles"),
+      "@components": path.resolve(__dirname, "src/components"),
+      "@pages": path.resolve(__dirname,"src/pages"),
+      "@containers": path.resolve(__dirname,"src/containers"),
+      "@logos": path.resolve(__dirname, "src/assets/logos"),
+      "@icons": path.resolve(__dirname, "src/assets/icons"),
+    },
   },
-  mode: 'development',
+  mode: "development",
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader',
-        }
+          loader: "babel-loader",
+        },
       },
       {
         test: /\.html$/,
-        use: [
-          { loader: 'html-loader' }
-        ]
+        use: [{ loader: "html-loader" }],
       },
       {
-        test: /\.css$/i,
-        use: [
-          'style-loader',
-          'css-loader',
-        ]
-      }
-    ]
+        test: /\.(css|scss)$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './public/index.html',
-      filename: './index.html'
+      template: "./public/index.html",
+      filename: "./index.html",
     }),
     new MiniCssExtractPlugin({
-      filename: '[name].css'
-    })
+      filename: "[name].css",
+    }),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, "src", "assets/logos"),
+          to: "assets/logos",
+        },
+      ],
+    }),
   ],
-}
+};
--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const CopyPlugin = require("copy-webpack-plugin");
 const TerserPlugin = require('terser-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
@@ -15,9 +16,13 @@ module.exports = {
   resolve: {
     extensions: ['.js', '.jsx'],
     alias: {
-      '@components': path.resolve(__dirname, 'src/components/'),
-      '@styles': path.resolve(__dirname, 'src/styles/')
-    }
+      "@styles": path.resolve(__dirname, "src/styles"),
+      "@components": path.resolve(__dirname, "src/components"),
+      "@pages": path.resolve(__dirname,"src/pages"),
+      "@containers": path.resolve(__dirname,"src/containers"),
+      "@logos": path.resolve(__dirname, "src/assets/logos"),
+      "@icons": path.resolve(__dirname, "src/assets/icons"),
+    },
   },
   mode: 'production',
   module: {
@@ -36,12 +41,13 @@ module.exports = {
         ]
       },
       {
-        test: /\.css$/i,
-        use: [
-          'style-loader',
-          'css-loader',
-        ]
-      }
+        test: /\.(css|scss)$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
     ]
   },
   plugins: [
@@ -51,6 +57,14 @@ module.exports = {
     }),
     new MiniCssExtractPlugin({
       filename: '[name].css'
+    }),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, "src", "assets/logos"),
+          to: "assets/logos",
+        },
+      ],
     }),
     new CleanWebpackPlugin(),
   ],
